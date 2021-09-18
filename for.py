26f937e1ac4b8efcac92b61898a2fc16d60ce0fb
Sebasfc10 <79613872+Sebasfc10@users.noreply.github.com>
2021-09-18 18:19:21 -0500

    ejercicio 2 - zoologico

--- for.py
+++ for.py
@@ -23,3 +23,43 @@ if c==7 or c==8:
 if c==9 or c==0:
     print('es azul')
     
+"""
+Un Zoólogo pretende determinar el porcentaje de animales que hay en las
+siguiente categorias de edades: 0 a 1 año, de mas de 1 año y menos de 3 y
+de 3 o mas años. El zoológico todavía no está seguro del animal que va
+estudiar. Si se decide por elefantes solo tomará una muestra de 20 de ellos;
+si se decide por jirafas, tomara 15 de muestras y si son chompancés tomará
+40.
+"""
+animal = str(input('Cual animal ingresara? elefante? jirafa? chimpance?: '))
+edad = int(input('Ingrese el rango de edad: 0 a 1 año, mas de 1 año y menos de 3 años y de 3 o mas años: '))
+p1 = 0
+p2 = 0
+p3 = 0
+if animal == 'elefante':
+    if edad >= 0 and edad <= 1:
+        p1 = p1 + 1
+    if edad > 1 and edad < 3:
+        p2 = p2 + 1
+    if edad > 3:
+        p3 = p3 + 1
+    print(f'su promedio es: {(p1/40)*100}')
+if animal == 'jirafa':
+    if edad >= 0 and edad <= 1:
+        p1 = p1 + 1
+    if edad > 1 and edad < 3:
+        p2 = p2 + 1
+    if edad > 3:
+        p3 = p3 + 1
+    print(f'su promedio es: {(p1/15)*100}')
+if animal == 'chimpance':
+    if edad >= 0 and edad <= 1:
+        p1 = p1 + 1
+    if edad > 1 and edad < 3:
+        p2 = p2 + 1
+    if edad > 3:
+        p3 = p3 + 1
+    print(f'su promedio es: {(p1/40)*100}')
+
+        
+    
